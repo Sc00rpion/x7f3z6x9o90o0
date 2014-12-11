3578833f7a25347f4ec1a611b371709351c65220
Sc00rpion <skarbekm@ee.pw.edu.pl>
2014-12-11 11:52:35 +0100

    Ogólny zarys modułu aproksymacji
    Ogólny zarys modułu aproksymacji wielomianami Hermite'a (nagłówki
    funkcji)

---
+++ aproksymatorhermite.c
@@ -0,0 +1,41 @@
+#include "makespl.h"
+#include "piv_ge_solver.h"
+#include "aproksymatorhermite.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+
+/* Funkcja bazowa -wielomian Hermite'a */
+double
+hi(int stopien, double x)
+{
+
+}
+
+/* Pierwsza pochodna hi */
+double
+dhi(int stopien, double x)
+{
+
+}
+
+/* Druga pochodna hi */
+double
+d2hi(int stopien, double x)
+{
+
+}
+
+/* Trzecia pochodna hi */
+double
+d3hi(int stopien, double x)
+{
+
+}
+
+void
+make_spl(points_t * pts, spline_t * spl)
+{
+
+}
---
+++ aproksymatorhermite.h
@@ -0,0 +1,15 @@
+#ifndef APRO_HERMIT
+#define APRO_HERMIT
+
+double
+hi(int stopien, double x);
+double
+dhi(int stopien, double x);
+double
+d2hi(int stopien, double x);
+double
+d3hi(int stopien, double x);
+
+
+
+#endif
